10636980e5566be3eb5c19ba22d30352cbf45b3b
Vittorio Antonicelli <vittorio.antonicelli@mail.polimi.it>
2020-04-13 16:48:35 +0200

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,356 @@
+const OrderData = {
+  data: [
+    {
+      createdAt: "2020-03-23T17:33:05.134000",
+      customer: {
+        address: {
+          description: "Viale Sondrio, 7, Milano, MI, 20124",
+          geoPoint: {
+            latitude: 45.49073,
+            longitude: 9.19879
+          },
+          normalized: true
+        },
+        billingAddress: {
+          description: "Viale Sondrio, 7, Milano, MI, 20124",
+          geoPoint: {
+            latitude: 45.49073,
+            longitude: 9.19879
+          },
+          normalized: true
+        },
+        businessName: "Borearco Enterprice",
+        email: "marceto4@gmil.com",
+        name: "Marco",
+        note: "Admin",
+        phoneNumber: "5554334222",
+        surname: "Betto",
+        vatNumber: "888855654545"
+      },
+      id: "5e78f2d1a2d9310b7c555dec",
+      lastUpdateAt: "2020-03-23T17:33:05.134000",
+      packages: [
+        {
+          dimension: {
+            depth: "40",
+            height: "40",
+            width: "40"
+          },
+          fragile: false,
+          note: "notes",
+          permeable: false,
+          unflippable: false,
+          unstackable: true,
+          weight: "10"
+        }
+      ],
+      receiver: {
+        address: {
+          description: "Piazza Duomo, 17, Milano, MI, Italia",
+          geoPoint: {
+            latitude: 45.4645968,
+            longitude: 9.188307199999999
+          }
+        },
+        email: "marcetto@studenolito.it",
+        name: "giorgio",
+        note: "notes",
+        phoneNumber: "1114334222",
+        surname: "giorgio"
+      },
+      serviceOption: {
+        deliveryDatetime: "2020-03-26T20:00:00",
+        id: "5e6687415bc3bcab89f43f2c",
+        price: 4.99,
+        type: "STD",
+        width: 11
+      },
+      trackingHistory: [
+        {
+          owner: "SYSTEM",
+          status: "CREATED",
+          updateAt: "2020-03-23T17:33:05.134000"
+        },
+        {
+          owner: "639414459",
+          status: "IN_TRANSIT",
+          updateAt: "2020-03-26T20:40:21.290000"
+        },
+        {
+          owner: "639414459",
+          status: "COMPLETED",
+          updateAt: "2020-03-26T20:40:43.144000"
+        }
+      ],
+      trackingNumber: "RL7cIF-JTzs"
+    },
+    {
+      createdAt: "2020-03-25T14:29:32.018000",
+      customer: {
+        address: {
+          description: "Viale Sondrio, 7, Milano, MI, 20124",
+          geoPoint: {
+            latitude: 45.49073,
+            longitude: 9.19879
+          },
+          normalized: true
+        },
+        billingAddress: {
+          description: "Viale Sondrio, 7, Milano, MI, 20124",
+          geoPoint: {
+            latitude: 45.49073,
+            longitude: 9.19879
+          },
+          normalized: true
+        },
+        businessName: "Borerco Enterprice",
+        email: "marett@gmil.com",
+        name: "Marco",
+        note: "Admin",
+        phoneNumber: "5554334222",
+        surname: "Boretto",
+        vatNumber: "985555654545"
+      },
+      id: "5e7b6adca2d9411a4b566df4",
+      lastUpdateAt: "2020-03-25T14:29:32.018000",
+      packages: [
+        {
+          dimension: {
+            depth: "40",
+            height: "60",
+            width: "40"
+          },
+          fragile: true,
+          note: "3, di cui 1 surg",
+          permeable: false,
+          unflippable: true,
+          unstackable: false,
+          weight: "10"
+        }
+      ],
+      receiver: {
+        address: {
+          description: "Via Torino, 33, Milano, MI, Italia",
+          geoPoint: {
+            latitude: 45.4624232,
+            longitude: 9.186412899999999
+          }
+        },
+        email: "k@k.it",
+        name: "Cara",
+        note: "bla bla",
+        phoneNumber: "1234567222",
+        surname: "Ferroni"
+      },
+      serviceOption: {
+        deliveryDatetime: "2020-03-26T20:00:00",
+        id: "5e7a438784dbb3a5aa216be6",
+        lsp2Services: ["CDM_STD_w10_dim[120, 40, 40]_dist18_Af2_At1"],
+        price: 7.99,
+        type: "STD",
+        width: 11
+      },
+      trackingHistory: [
+        {
+          owner: "SYSTEM",
+          status: "CREATED",
+          updateAt: "2020-03-25T14:29:32.018000"
+        },
+        {
+          owner: "966181910",
+          status: "IN_TRANSIT",
+          updateAt: "2020-03-26T19:41:17.973000"
+        },
+        {
+          owner: "966181910",
+          status: "COMPLETED",
+          updateAt: "2020-03-26T19:43:18.679000"
+        }
+      ],
+      trackingNumber: "XURKaVdS1_k"
+    },
+    {
+      createdAt: "2020-03-25T15:44:29.980000",
+      customer: {
+        address: {
+          description: "Viale Sondrio, 7, Milano, MI, 20124",
+          geoPoint: {
+            latitude: 45.49073,
+            longitude: 9.19879
+          },
+          normalized: true
+        },
+        billingAddress: {
+          description: "Viale Sondrio, 7, Milano, MI, 20124",
+          geoPoint: {
+            latitude: 45.49073,
+            longitude: 9.19879
+          },
+          normalized: true
+        },
+        businessName: "Borerco Enterprice",
+        email: "marret4@gmil.com",
+        name: "Marco",
+        note: "Admrrin",
+        phoneNumber: "3454334222",
+        surname: "Boretto",
+        vatNumber: "988855654545"
+      },
+      id: "5e7b7c5ea2d9221p4b566df7",
+      lastUpdateAt: "2020-03-25T15:44:29.980000",
+      packages: [
+        {
+          dimension: {
+            depth: "40",
+            height: "60",
+            width: "40"
+          },
+          fragile: true,
+          note: "",
+          permeable: false,
+          unflippable: true,
+          unstackable: true,
+          weight: "10"
+        }
+      ],
+      receiver: {
+        address: {
+          description:
+            "Via Privata Don Bartolomeo Grazioli, 1, Milano, MI, Italia",
+          geoPoint: {
+            latitude: 45.5100809,
+            longitude: 9.1759472
+          }
+        },
+        email: "k@k.it",
+        name: "gigio",
+        note: "Gio Giogio",
+        phoneNumber: "123456222",
+        surname: "gio"
+      },
+      serviceOption: {
+        deliveryDatetime: "2020-03-26T20:00:00",
+        id: "5e7a438784dbb3a5aa216be6",
+        lsp2Services: ["CDM_STD_w10_dim[120, 40, 40]_dist18_Af2_At2"],
+        price: 8.49,
+        type: "STD",
+        width: 11
+      },
+      trackingHistory: [
+        {
+          owner: "SYSTEM",
+          status: "CREATED",
+          updateAt: "2020-03-25T15:44:29.980000"
+        },
+        {
+          owner: "966181910",
+          status: "IN_TRANSIT",
+          updateAt: "2020-03-26T17:11:51.157000"
+        },
+        {
+          owner: "966181910",
+          status: "DELIVERED",
+          updateAt: "2020-03-26T17:13:11.095000"
+        },
+        {
+          owner: "966181910",
+          status: "IN_TRANSIT",
+          updateAt: "2020-03-26T17:32:17.991000"
+        },
+        {
+          owner: "966181910",
+          status: "COMPLETED",
+          updateAt: "2020-03-26T17:33:17.401000"
+        }
+      ],
+      trackingNumber: "L-HCO0raxlM"
+    },
+    {
+      createdAt: "2020-03-29T12:29:30.933000",
+      customer: {
+        address: {
+          description: "Viale Sondrio, 7, Milano, MI, 20124",
+          geoPoint: {
+            latitude: 45.49073,
+            longitude: 9.19879
+          },
+          normalized: true
+        },
+        billingAddress: {
+          description: "Viale Sondrio, 7, Milano, MI, 20124",
+          geoPoint: {
+            latitude: 45.49073,
+            longitude: 9.19879
+          },
+          normalized: true
+        },
+        businessName: "Boret Marco Enterprice",
+        email: "marbor@gmil.com",
+        name: "Marco",
+        note: "Admin",
+        phoneNumber: "44543347222",
+        surname: "Boretto",
+        vatNumber: "008855654545"
+      },
+      id: "5e8094cd307c7f39d255a86c",
+      lastUpdateAt: "2020-03-29T12:29:30.933000",
+      packages: [
+        {
+          dimension: {
+            depth: "40",
+            height: "60",
+            width: "40"
+          },
+          fragile: true,
+          note: "nota spedizione",
+          permeable: false,
+          unflippable: true,
+          unstackable: false,
+          weight: "10"
+        }
+      ],
+      receiver: {
+        address: {
+          description: "Via Palestro, 33, Milano, MI, Italia",
+          geoPoint: {
+            latitude: 45.4728131,
+            longitude: 9.1993359
+          }
+        },
+        email: "pierluncla@gmil.com",
+        name: "Piluigi",
+        note: "5",
+        phoneNumber: "33223765222",
+        surname: "Gincooola"
+      },
+      serviceOption: {
+        deliveryDatetime: "2020-04-02T20:00:00",
+        id: "5e7b9a3b14bcd9716ef7d8e7",
+        lsp2Services: ["CDM_STD_w10_dim[120, 40, 40]_dist18_Af2_At1"],
+        price: 7.99,
+        type: "STD",
+        width: 11
+      },
+      trackingHistory: [
+        {
+          owner: "SYSTEM",
+          status: "CREATED",
+          updateAt: "2020-03-29T12:29:30.933000"
+        },
+        {
+          owner: "272249988",
+          status: "IN_TRANSIT",
+          updateAt: "2020-03-29T12:40:57.166000"
+        },
+        {
+          owner: "272249988",
+          status: "COMPLETED",
+          updateAt: "2020-03-29T12:41:05.905000"
+        }
+      ],
+      trackingNumber: "pbqgU87xvNo"
+    }
+  ]
+};
+
+export default OrderData;
---
+++ src/index.js
@@ -0,0 +1,268 @@
+import React from "react";
+import { render } from "react-dom";
+import { slideDown, slideUp } from "./anim";
+import "./style.css";
+import OrderData from "./data";
+
+function capitalize(str) {
+  return str
+    .split(" ")
+    .map(s => {
+      return s.charAt(0).toUpperCase() + s.substr(1);
+    })
+    .join(" ");
+}
+
+class OrderTableRow extends React.Component {
+  state = { expanded: false };
+
+  toggleExpander = e => {
+    if (e.target.type === "checkbox") return;
+
+    if (!this.state.expanded) {
+      this.setState({ expanded: true }, () => {
+        if (this.refs.expanderBody) {
+          slideDown(this.refs.expanderBody);
+        }
+      });
+    } else {
+      slideUp(this.refs.expanderBody, {
+        onComplete: () => {
+          this.setState({ expanded: false });
+        }
+      });
+    }
+  };
+
+  render() {
+    const { order } = this.props;
+
+    let servOptMap = new Map([
+      ["STD", "Standard"],
+      ["URG", "Urgente"],
+      ["TS", "Fascia Oraria"]
+    ]);
+    let ordStatMap = new Map([
+      ["CREATED", "Creato"],
+      ["IN_TRANSIT", "In transito"],
+      ["COMPLETED", "Completato"]
+    ]);
+
+    var creationDate = new Date(order.createdAt);
+    var deliveryDate = new Date(order.serviceOption.deliveryDatetime);
+    var deliveryDateBegin = new Date(deliveryDate.getTime());
+    deliveryDateBegin.setHours(
+      deliveryDateBegin.getHours() - order.serviceOption.width
+    );
+
+    return [
+      <tr key="main" onClick={this.toggleExpander}>
+        <td className="uk-text-nowrap">{this.props.index}.</td>
+        <td>
+          {ordStatMap.get(
+            order.trackingHistory[order.trackingHistory.length - 1].status
+          )}
+        </td>
+        <td>
+          {creationDate.getDate() +
+            "/" +
+            (creationDate.getMonth() + 1) +
+            "/" +
+            creationDate.getFullYear() +
+            " " +
+            creationDate.toLocaleTimeString(["it-IT"], {
+              hour: "2-digit",
+              minute: "2-digit"
+            })}
+        </td>
+        <td>
+          {capitalize(order.receiver.name + " " + order.receiver.surname)}
+        </td>
+        <td>{servOptMap.get(order.serviceOption.type)}</td>
+        <td>
+          {deliveryDate.getDate() +
+            "/" +
+            (deliveryDate.getMonth() + 1) +
+            "/" +
+            deliveryDate.getFullYear()}
+          <br />
+          <small>
+            {deliveryDateBegin.toLocaleTimeString(["it-IT"], {
+              hour: "2-digit",
+              minute: "2-digit"
+            }) +
+              " - " +
+              deliveryDate.toLocaleTimeString(["it-IT"], {
+                hour: "2-digit",
+                minute: "2-digit"
+              })}
+          </small>
+        </td>
+      </tr>,
+      this.state.expanded && (
+        <tr className="expandable" key="tr-expander">
+          <td className="uk-background-muted" colSpan={6}>
+            <div ref="expanderBody" className="inner uk-grid">
+              <div className="uk-width-1-2">
+                <p>
+                  Codice tracking
+                  <br />
+                  {order.trackingNumber}
+                </p>
+                <p>
+                  Costo consegna
+                  <br />
+                  {order.serviceOption.price + "€"}
+                </p>
+                <p>
+                  <small>Dati pacco</small>
+                  <br />
+                  Dimensioni: {order.packages[0].dimension.height} x{" "}
+                  {order.packages[0].dimension.width} x{" "}
+                  {order.packages[0].dimension.depth} cm
+                  <br />
+                  Peso: {order.packages[0].weight} kg
+                  {order.packages[0].permeable ? (
+                    <span>
+                      <br />
+                      Sensibile all'acqua
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                  {order.packages[0].unflippable ? (
+                    <span>
+                      <br />
+                      Non ribaltabile
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                  {order.packages[0].unstackable ? (
+                    <span>
+                      <br />
+                      Non impilabile
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                  {order.packages[0].fragile ? (
+                    <span>
+                      <br />
+                      Fragile
+                    </span>
+                  ) : (
+                    ""
+                  )}
+                </p>
+                {order.packages[0].note.length > 0 ? (
+                  <p>
+                    Note
+                    <br />
+                    <i>{order.packages[0].note}</i>
+                  </p>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="uk-width-1-2">
+                <p>
+                  Destinatario
+                  <br />
+                  <i>
+                    {capitalize(
+                      order.receiver.name + " " + order.receiver.surname
+                    )}
+                  </i>
+                </p>
+                <p>
+                  Indirizzo
+                  <br />
+                  <i>{capitalize(order.receiver.address.description)}</i>
+                </p>
+                <p>
+                  Email
+                  <br />
+                  <i>{order.receiver.email}</i>
+                </p>
+                <p>
+                  Telefono
+                  <br />
+                  {order.receiver.phoneNumber}
+                </p>
+              </div>
+            </div>
+          </td>
+        </tr>
+      )
+    ];
+  }
+}
+
+class App extends React.Component {
+  state = { users: null };
+
+  componentDidMount() {
+    fetch("https://randomuser.me/api/1.1/?results=15")
+      .then(response => response.json())
+      .then(data => {
+        console.log("data fetched: [unused]");
+        console.log(data);
+        console.log("loading data from file to app state: [unused]");
+        console.log(OrderData);
+        this.setState({ orders: OrderData.data });
+      });
+  }
+
+  render() {
+    //const { orders } = this.state;
+    const orders = this.state.orders;
+    console.log("orders in app state: [unused]");
+    console.log(orders);
+
+    const orders2 = OrderData.data;
+    console.log("orders2: [currently used]");
+    console.log(orders2);
+
+    const isLoading = orders2 === null;
+    return (
+      <main>
+        <div className="table-container">
+          <div className="uk-overflow-auto">
+            <table className="uk-table uk-table-hover uk-table-middle uk-table-divider">
+              <thead>
+                <tr>
+                  <th className="uk-table-shrink" />
+                  <th className="uk-table-shrink">STATO</th>
+                  <th>DATA CREAZIONE</th>
+                  <th>DESTINATARIO</th>
+                  <th>TIPO DI CONSEGNA</th>
+                  <th>FASCIA DI CONSEGNA</th>
+                </tr>
+              </thead>
+              <tbody>
+                {isLoading ? (
+                  <tr>
+                    <td colSpan={6} className="uk-text-center">
+                      <em className="uk-text-muted">Loading...</em>
+                    </td>
+                  </tr>
+                ) : (
+                  orders2.map((order, index) => (
+                    <OrderTableRow
+                      key={index}
+                      index={index + 1}
+                      order={order}
+                    />
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </main>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
